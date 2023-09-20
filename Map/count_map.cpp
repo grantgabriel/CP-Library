1d87c4b3260ca2f86579deede639674af8f459c1
grantgabriel <grantgabriel30@gmail.com>
2023-09-20 20:15:50 +0700

    Minor update and add readme

--- Map/count_map.cpp
+++ Map/count_map.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 using namespace std;
 
+// TODO : Make count_map for array, and also for immediate input.
+
+// Counter for elements in a vector, stored in map. Count is accessed by element indexing. 
 template<class T>
 map<T, T> count_map(vector<T> &V) {
     map<T, T> counter;
