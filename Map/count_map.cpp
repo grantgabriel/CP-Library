e9be6c3d3ed639ee68211f28b55f78565a8700f9
grantgabriel <grantgabriel30@gmail.com>
2023-10-01 14:02:49 +0700

    Add array as an input for map counter

--- Map/count_map.cpp
+++ Map/count_map.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // Counter for elements in a vector, stored in map. Count is accessed by element indexing. 
 template<class T>
-map<T, T> count_map(vector<T> &V) {
+map<T, T> count_mapvec(vector<T> &V) {
     map<T, T> counter;
     for(auto &v : V) {
         counter[v]++;
@@ -16,9 +16,19 @@ map<T, T> count_map(vector<T> &V) {
     return counter;
 }
 
+template<class T>
+map<T, T> count_maparr(T arr[], T size) {
+    map<T, T> counter;
+    for(unsigned long long int i = 0; i < size; i++) {
+        counter[v]++;
+    }
+
+    return counter;
+}
+
 int main() {
-    vector<int> nums = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
-    map<int, int> mapping = count_map(nums);
+    int nums[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+    map<int, int> mapping = count_maparr(nums, sizeof(nums) / sizeof(nums[0]));
 
     for(const auto& pair : mapping) {
         cout << pair.first << ' ' << pair.second << endl;
